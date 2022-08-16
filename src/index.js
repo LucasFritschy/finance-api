51085986a544175cb4409ba3301bf7dc8d5352d7
Lucas Fritschy <lucasfritschy@gmail.com>
2022-08-16 09:25:09 -0300

    first routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'ok' })
+
+const customers = []
+
+app.get('/account', (req, res) => {
+  return res.status(200).json({ customers })
+})
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body
+  const uuid = uuidv4()
+
+  customers.push({
+    cpf,
+    name,
+    id: uuid,
+    statements: []
+  })
+
+  return res.status(201).send()
 })
 
 app.listen(3333)
