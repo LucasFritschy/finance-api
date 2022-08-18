90bf01c2ed7f52f186a4dee1e30bc9733bca9513
Lucas Fritschy <lucasfritschy@gmail.com>
2022-08-18 11:50:50 -0300

    add withdraw request

--- src/index.js
+++ src/index.js
@@ -22,10 +22,36 @@ function checkIfAccountIsValid(req, res, next) {
 }
 
 function checkIfAccountAlreadyExists(req, res, next) {
-  const { cpf, name } = req.body
+  const { cpf } = req.body
 
   if (customers.some((customer) => customer.cpf === cpf)) {
     return res.status(400).json({ error: 'Customer already exists!' })
+  }
+
+  next()
+}
+
+function getAccountBalance(req, res, next) {
+  const { customer } = req
+  const balance = customer.statements.reduce((acc, statement) => {
+    if (statement.type === 'deposit') {
+      acc += statement.amount
+    } else {
+      acc -= statement.amount
+    }
+    return acc
+  }, 0)
+
+  req.balance = balance
+  next()
+}
+
+function validateWithdraw(req, res, next) {
+  const { balance } = req
+  const { amount } = req.body
+
+  if (balance < amount) {
+    return res.status(400).json({ error: 'Insufficient funds on account' })
   }
 
   next()
@@ -62,7 +88,21 @@ app.post('/deposit/:cpf', checkIfAccountIsValid, (req, res) => {
     description,
     amount,
     created_at: new Date(),
-    type: 'Deposit'
+    type: 'deposit'
+  })
+
+  return res.status(201).send()
+})
+
+app.post('/withdraw/:cpf', checkIfAccountIsValid, getAccountBalance, validateWithdraw, (req, res) => {
+  const { customer } = req
+  const { description, amount } = req.body
+
+  customer.statements.push({
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'withdraw'
   })
 
   return res.status(201).send()
